049c19f252af8736471a33ef3c9a441b4e3fc7b5
parkjungkwan <pakjkwan@gmail.com>
2022-02-24 17:33:58 +0900

    17

--- components/Bmi.js
+++ components/Bmi.js
@@ -1,35 +1,52 @@
 import React, {useState} from 'react' 
 import Layout from '../containers/Layout';
 import axios  from 'axios';
+import { useLinkClickHandler } from 'react-router-dom';
 export default function Bmi(){
 
+    const [inputs, setInputs] = useState({
+        name: '', height: 0.0, weight: 0.0
+    })
     const [name, setName] = useState('')
-    const [height, setHeight] = useState(0.0)
-    const [weight, setWeight] = useState(0.0)
-    const getBmi = (e) => {
+
+    const handleChange = (e) => {
         e.preventDefault()
-        alert(` 버튼 클릭 `)
-        axios.get(`http://localhost:8080/member/bmi/aaa/180.5/80.5`)
+        /**const {value, name } = e.target;
+        setInputs({
+            ...inputs, [name]: value
+        })*/
+        alert(`입력한 이름 ${document.getElementById("name").value}`)
+        setName(document.getElementById("name").value)
+        
+    }
+    const handleClick = (e) => {
+        e.preventDefault()
+        alert(` 사용자이름: ${inputs["name"]}`)
+        /*
+        axios.get(`http://localhost:8080/member/bmi/김길동/180.5/80.5`)
             .then((res)=>{
                 alert(`답장이 도착했습니다 [내용] ${JSON.stringify(res.data)}`)
-            })
+            })*/
     }
 
 
-    return (<Layout><h1>Bmi폼</h1>
+    return (<Layout>
+        <form>
+        <h1>Bmi폼</h1>
    
     
     <div>
     <label><b>Username</b></label>
-    <input type="" /><br />
+    <input type="text" id="name" onChange={handleChange} /><br />
 
     <label htmlFor=""><b>height</b></label>
-    <input type="" /><br />
+    <input type="text" onChange={handleChange} /><br />
 
     <label htmlFor=""><b>weight</b></label>
-    <input type="button" onClick={getBmi} value="BMI 체크" /><br />
+    <input type="text" onChange={handleChange} /><br />
+    <button onClick={handleClick}>BMI 체크</button>
     </div> 
-    
+    </form>
    
     </Layout>)
 }
